a009e8f0ce449dd99690486211a34347565fd510
ka2jun8 <ka210n8@gmail.com>
2019-03-12 16:44:50 +0900

    feat: convert process env data for handlebars

--- src/util.ts
+++ src/util.ts
@@ -1,11 +1,18 @@
 import { default as Handlebars } from "handlebars";
 
+function toCamelCase(envStr: string) {
+  const l = envStr.toLowerCase();
+  return l.replace(/_./g, s => {
+    return s.charAt(1).toUpperCase();
+  });
+}
+
 export function convert(yaml: string): string {
-  const data = {
-    hostUrl: process.env.HOST_URL || "http://localhost:3000",
-    password: process.env.PASSWORD || "passw0rd",
-    userId: process.env.USER_ID || "test@example.com"
-  };
+  const data = {};
+  Object.keys(process.env).forEach(envkey => {
+    const key = toCamelCase(envkey);
+    data[key] = process.env[envkey];
+  });
   const template = Handlebars.compile(yaml);
   const converted = template(data);
   return converted;
